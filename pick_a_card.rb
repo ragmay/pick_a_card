6f8b0c19c47ab898f20a2a7a0b19e5ff1b39932e
ragmay <5567960@gmail.com>
2020-08-07 12:44:53 +0300

    Замена мастей со слов на соответствующие значки

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # буби, черви, крести, пики
+suits = ['♠', '♥', '♣', '♦'] # буби, черви, крести, пики
 
 puts "#{values.sample} of #{suits.sample}"
